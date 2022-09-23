352f557376c063261a81fc32ab784d651f483d8e
NTSon <thaisonbk2020@gmail.com>
2022-09-23 11:01:10 +0700

    Setup basic w/ meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    origin: "http://127.0.0.1:8080",
-  },
+  // Document way but it not work
+  // server: {
+  //   origin: "http://127.0.0.1:8080",
+  // },
+  
+  // server: {
+  //   port: 3030, // custom port for Vite
+  // },
+  // preview: {
+  //   port: 8080,
+  // },
 });
